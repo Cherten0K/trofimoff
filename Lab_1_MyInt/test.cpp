d6bd3e9d2c1a5de076c77bc4df6089dddec1ad3f
Cherten0K <ivan_cheretaev@inbox.ru>
2015-03-25 11:44:00 +0500

    delete message from test

--- Lab_1_MyInt/test.cpp
+++ Lab_1_MyInt/test.cpp
@@ -6,8 +6,7 @@ void test(void){
 	try{
 		MyInt test_unar_minus(45);
 		int result_unar_minus = -45;
-		if (-(test_unar_minus).N() == result_unar_minus)
-			cout << "Test for the unary subtraction passed" << endl;
+		if (-(test_unar_minus).N() == result_unar_minus){}
 		else
 			cout << endl << "!!!Test fails unary subtraction" << endl << endl;
 	}
@@ -21,7 +20,6 @@ void test(void){
 		cout << endl << "*** Test for overflow unary subtraction fails ***" << endl << endl;
 	}
 	catch (OverFlow *of){
-		cout << "Test for overflow unary subtraction passed" << endl;
 	}
 	catch (...){
 		cout << endl << "!!!Test for overflow unary subtraction failed: unexpected exception" << endl << endl;
@@ -31,8 +29,7 @@ void test(void){
 	try{
 		MyInt test_addition_A(45), test_addition_B(56);
 		int result_addition = 101;
-		if ((test_addition_A + test_addition_B).N() == result_addition)
-			cout << "Test for the addition passed" << endl;
+		if ((test_addition_A + test_addition_B).N() == result_addition){}
 		else
 			cout << endl << "!!!Test fails addition" << endl;
 	}
@@ -45,7 +42,6 @@ void test(void){
 		cout << endl << "*** Test for overflow when adding fails ***" << endl << endl;
 	}
 	catch (OverFlow *of){
-		cout << "Test for overflow when adding passed" << endl;
 	}
 	catch (...){
 		cout << endl << "!!!Test for overflow when adding failed: unexpected exception" << endl << endl;
@@ -55,8 +51,7 @@ void test(void){
 	try{
 		MyInt test_subtraction_A(45), test_subtraction_B(56);
 		int result_subtraction = -11;
-		if ((test_subtraction_A - test_subtraction_B).N() == result_subtraction)
-			cout << "Test for the subtraction passed" << endl;
+		if ((test_subtraction_A - test_subtraction_B).N() == result_subtraction){}
 		else
 			cout << endl << "!!!Test fails subtraction" << endl;
 	}
@@ -70,7 +65,6 @@ void test(void){
 		cout << endl << "*** Test for overflow in subtraction fails ***" << endl << endl;
 	}
 	catch (OverFlow *of){
-		cout << "Test for overflow in subtraction passed" << endl;
 	}
 	catch (...){
 		cout << endl << "!!!Test for overflow in subtraction failed: unexpected exception" << endl << endl;
@@ -79,8 +73,7 @@ void test(void){
 	try{
 		MyInt test_division_A(45), test_division_B(5);
 		int result_division = 9;
-		if ((test_division_A / test_division_B).N() == result_division)
-			cout << "Test for the division passed" << endl;
+		if ((test_division_A / test_division_B).N() == result_division){}
 		else
 			cout << endl << "!!!Test fails division" << endl;
 	}
@@ -94,7 +87,6 @@ void test(void){
 		cout << endl << "*** Test for division by zero fails ***" << endl << endl;
 	}
 	catch (DivideByZero *of){
-		cout << "Test for division by zero passed" << endl;
 	}
 	catch (...){
 		cout << endl << "!!!Test for division by zero failed: unexpected exception" << endl << endl;
@@ -104,8 +96,7 @@ void test(void){
 	try{
 		MyInt test_multiplication_A(45), test_multiplication_B(5);
 		int result_multiplication = 225;
-		if ((test_multiplication_A * test_multiplication_B).N() == result_multiplication)
-			cout << "Test for the multiplication passed" << endl;
+		if ((test_multiplication_A * test_multiplication_B).N() == result_multiplication){}
 		else
 			cout << endl << "!!!Test fails multiplication" << endl << endl;
 	}
@@ -119,7 +110,6 @@ void test(void){
 		cout << endl << "*** Test for overflow in multiplition fails ***" << endl << endl;
 	}
 	catch (OverFlow *of){
-		cout << "Test for overflow in multiplication passed" << endl;
 	}
 	catch (...){
 		cout << endl << "Test for overflow in multiplication failed: unexpected exception" << endl << endl;
@@ -128,8 +118,7 @@ void test(void){
 	try{
 		MyInt test_division_with_mod_A(42), test_division_with_mod_B(5);
 		int result_division_with_mod = 2;
-		if ((test_division_with_mod_A % test_division_with_mod_B).N() == result_division_with_mod)
-			cout << "Test for modulo passed" << endl;
+		if ((test_division_with_mod_A % test_division_with_mod_B).N() == result_division_with_mod){}
 		else
 			cout << endl << "!!!Test fails divition" << endl;
 	}
@@ -143,7 +132,6 @@ void test(void){
 		cout << endl << "!!!Test for modulo exceptions fails " << endl << endl;
 	}
 	catch (DivideByZero *of){
-		cout << "Test for modulo by zero passed" << endl;
 	}
 	catch (...){
 		cout << endl << "!!!Test modulo by zero fails: unexpected exception" << endl << endl;
@@ -154,8 +142,7 @@ void test(void){
 		MyInt test_addition_A(45), test_addition_B(56);
 		int result_addition = 101;
 		test_addition_A += test_addition_B;
-		if (test_addition_A.N() == result_addition)
-			cout << "Passed the test for the addition with assignment" << endl;
+		if (test_addition_A.N() == result_addition){}
 		else
 			cout << endl << "!!!Test fails addition with assignment" << endl;
 	}
@@ -169,7 +156,6 @@ void test(void){
 		cout << endl << "*** Test for overflow when adding with assignment fails ***" << endl << endl;
 	}
 	catch (OverFlow *of){
-		cout << "Test for overflow when adding with assignment passed" << endl;
 	}
 	catch (...){
 		cout << endl << "!!!Test for overflow when adding with assignment failed" << endl << endl;
@@ -180,8 +166,7 @@ void test(void){
 		MyInt test_subtraction_A(45), test_subtraction_B(56);
 		int result_subtraction = -11;
 		test_subtraction_A -= test_subtraction_B;
-		if (test_subtraction_A.N() == result_subtraction)
-			cout << "Passed the test for the subtraction with assignment" << endl;
+		if (test_subtraction_A.N() == result_subtraction){}
 		else
 			cout << endl << "!!!Test fails subtraction with assignment" << endl;
 	}
@@ -195,7 +180,6 @@ void test(void){
 		cout << endl << "*** Test for overflow in subtraction with assignment fails ***" << endl << endl;
 	}
 	catch (OverFlow *of){
-		cout << "Test for overflow in subtraction with assignment passed" << endl;
 	}
 	catch (...){
 		cout << endl << "!!!Test for overflow in substraction with assignment failed: unexpected exception" << endl << endl;
@@ -206,8 +190,7 @@ void test(void){
 		MyInt test_division_A(45), test_division_B(5);
 		int result_division = 9;
 		test_division_A /= test_division_B;
-		if (test_division_A.N() == result_division)
-			cout << "Passed the test for the divition with assignment" << endl;
+		if (test_division_A.N() == result_division){}
 		else
 			cout << endl << "!!!Test fails divition with assignment" << endl;
 	}
@@ -221,7 +204,6 @@ void test(void){
 		cout << endl << "*** Test for division by zero with assignment fails ***" << endl << endl;
 	}
 	catch (DivideByZero *of){
-		cout << "Test for division by zero with assignment passed" << endl;
 	}
 	catch (...){
 		cout << endl << "!!!Test for division by zero with assignment failed: unexpected exception" << endl << endl;
@@ -232,8 +214,7 @@ void test(void){
 		MyInt test_multiplication_A(45), test_multiplication_B(5);
 		int result_multiplication = 225;
 		test_multiplication_A *= test_multiplication_B;
-		if (test_multiplication_A.N() == result_multiplication)
-			cout << "Passed the test for the multiplication with assignment" << endl;
+		if (test_multiplication_A.N() == result_multiplication){}
 		else
 			cout << endl << "!!!Test fails multiplication with assignment" << endl << endl;
 	}
@@ -247,7 +228,6 @@ void test(void){
 		cout << endl << "*** Test for overflow in multiplition with assignment fails ***" << endl << endl;
 	}
 	catch (OverFlow *of){
-		cout << "Test for overflow in multiplication with assignment passed" << endl;
 	}
 	catch (...){
 		cout << endl << "!!!Test for multiplication with assignment failed: unexpected exception" << endl << endl;
@@ -258,8 +238,7 @@ void test(void){
 		MyInt test_division_with_mod_A(42), test_division_with_mod_B(5);
 		int result_division_with_mod = 2;
 		test_division_with_mod_A %= test_division_with_mod_B;
-		if (test_division_with_mod_A.N() == result_division_with_mod)
-			cout << "Passed the test for modulo with assignment with " << endl;
+		if (test_division_with_mod_A.N() == result_division_with_mod){}
 		else
 			cout << endl << "!!!Test fails modulo with assignment" << endl;
 	}
@@ -273,7 +252,6 @@ void test(void){
 		cout << endl << "*** Test for division by zero with assignment fails ***" << endl << endl;
 	}
 	catch (DivideByZero *of){
-		cout << "Test for division by zero  with assignment passed" << endl;
 	}
 	catch (...){
 		cout << endl << "!!!Test fails modulo with assignment" << endl << endl;
@@ -283,8 +261,7 @@ void test(void){
 		MyInt test_preincr(6);
 		int result_preincr = 7;
 		++test_preincr;
-		if (test_preincr.N() == result_preincr)
-			cout << "Test for preincrement passed" << endl;
+		if (test_preincr.N() == result_preincr){}
 		else
 			cout << endl << "!!!Test for preincrement fails" << endl << endl;
 	}
@@ -298,7 +275,6 @@ void test(void){
 		cout << endl << "!!!Test for overflow in preincrement fails" << endl << endl;
 	}
 	catch (OverFlow *of){
-		cout << "Test for overflow in preincrement passed" << endl;
 	}
 	catch (...){
 		cout << endl << "!!! Test for overflow in preincrement fails: unexpected exception" << endl << endl;
@@ -308,8 +284,7 @@ void test(void){
 	try{
 		MyInt test_postincr(6);
 		int result_postincr = 7;
-		if ((test_postincr++).N() == result_postincr)
-			cout << "Test for postincrement passed" << endl;
+		if ((test_postincr++).N() == result_postincr){}
 		else
 			cout << endl << "!!!Test for postincrement fails" << endl << endl;
 	}
@@ -323,7 +298,6 @@ void test(void){
 		cout << endl << "!!!Test for overflow in postincrement fails" << endl << endl;
 	}
 	catch (OverFlow *of){
-		cout << "Test for overflow in postincrement passed" << endl;
 	}
 	catch (...){
 		cout << endl << "Test for overflow in postincrement fails: unexpected exception" << endl << endl;
@@ -334,8 +308,7 @@ void test(void){
 		MyInt test_predecr(6);
 		int result_predecr = 5;
 		--test_predecr;
-		if (test_predecr.N() == result_predecr)
-			cout << "Test for predecrement passed" << endl;
+		if (test_predecr.N() == result_predecr){}
 		else
 			cout << endl << "!!!Test for predecrement fails" << endl << endl;
 	}
@@ -349,7 +322,6 @@ void test(void){
 		cout << endl << "!!!Test for overflow in predecrement fails" << endl << endl;
 	}
 	catch (OverFlow *of){
-		cout << "Test for overflow in predecrement passed" << endl;
 	}
 	catch (...){
 		cout << endl << "!!! Test for overflow in predecrement fails: unexpected exception" << endl << endl;
@@ -359,8 +331,7 @@ void test(void){
 	try{
 		MyInt test_postdecr(6);
 		int result_postdecr = 5;
-		if ((test_postdecr--).N() == result_postdecr)
-			cout << "Test for postdecrement passed" << endl;
+		if ((test_postdecr--).N() == result_postdecr){}
 		else
 			cout << endl << "!!!Test for postdecrement fails" << endl << endl;
 	}
@@ -374,7 +345,6 @@ void test(void){
 		cout << endl << "!!!Test for overflow in postdecrement fails" << endl << endl;
 	}
 	catch (OverFlow *of){
-		cout << "Test for overflow in postdecrement passed" << endl;
 	}
 	catch (...){
 		cout << endl << "Test for overflow in postdecrement fails: unexpected exception" << endl << endl;
